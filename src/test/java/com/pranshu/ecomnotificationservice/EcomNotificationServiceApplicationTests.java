a86f863513451d0f9358d47ab83e3eddda08139b
Pranshu Chakraborty <pranshu.c1@gmail.com>
2024-09-15 19:03:16 +0530

    Initial commit

---
+++ src/test/java/com/pranshu/ecomnotificationservice/EcomNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranshu.ecomnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
